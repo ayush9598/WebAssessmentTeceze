8746f97a57b9b515300f0070ba6d503ae6cd4e72
Ayush Varshney <varshneyayush9415@gmail.com>
2024-10-09 14:10:47 +0000

    Committed Code

--- web/themes/custom/teceze/assets/main.js
+++ web/themes/custom/teceze/assets/main.js
@@ -1,1 +1,16 @@
-console.log('Inside');
+(function ($, Drupal) {
+    Drupal.behaviors.teceze = {
+      attach: function (context, settings) {
+        $('.testimonial-items-slider').slick({
+            dots: true,            // Shows navigation dots
+            infinite: true,        // Infinite loop
+            speed: 300,            // Transition speed
+            slidesToShow: 1,       // Number of slides to show
+            slidesToScroll: 1,     // Number of slides to scroll
+            arrows: true,
+            prevArrow: '<i class="fa-solid fa-angle-left"></i>',
+            nextArrow: '<i class="fa-solid fa-angle-right"></i>',
+          });
+      }
+    };
+  })(jQuery, Drupal);
