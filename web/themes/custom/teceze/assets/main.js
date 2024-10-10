71f017556abb677a02d43abd52c68900f1ce5e9f
Ayush Varshney <varshneyayush9415@gmail.com>
2024-10-10 09:13:30 +0000

    Committed Code

--- web/themes/custom/teceze/assets/main.js
+++ web/themes/custom/teceze/assets/main.js
@@ -11,6 +11,49 @@
             prevArrow: '<i class="fa-solid fa-angle-left"></i>',
             nextArrow: '<i class="fa-solid fa-angle-right"></i>',
           });
+        // Select the burger icon and navigation links
+        const menuIcon = document.getElementById('menu-icon');
+        const navLinks = document.getElementById('nav-links');
+
+        // Add click event listener to the burger icon
+        menuIcon.addEventListener('click', () => {
+          // Toggle the 'active' class on the navigation links to show/hide the menu
+          navLinks.classList.toggle('active');
+          // If the menu is active, disable scrolling by adding a class to body
+          if (navLinks.classList.contains('active')) {
+            document.body.classList.add('no-scroll');
+          } else {
+            document.body.classList.remove('no-scroll');
+          }
+        });
+        function initializeSlick() {
+          if ($(window).width() <= 768) {
+              if (!$('.plans').hasClass('slick-initialized')) {
+                  $('.plans').slick({
+                      // Your slick options here
+                      slidesToShow: 1,
+                      slidesToScroll: 1,
+                      dots: false,
+                      infinite: true,
+                      arrows: true,
+                      prevArrow: '<i class="fa-solid fa-angle-left"></i>',
+                      nextArrow: '<i class="fa-solid fa-angle-right"></i>', 
+                  });
+              }
+          } else {
+              if ($('.plans').hasClass('slick-initialized')) {
+                  $('.plans').slick('unslick'); // Destroy slick if initialized
+              }
+          }
+      }
+  
+      // Initialize on page load
+      initializeSlick();
+  
+      // Reinitialize on window resize
+      $(window).resize(function() {
+          initializeSlick();
+      });
       }
     };
   })(jQuery, Drupal);
